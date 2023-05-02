6faac89e4df0a2986992685b7cdf7a669c5007eb
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2023-05-01 21:49:14 -0400

    fix: rm unused batch transfer ERC1155

--- src/libraries/TransferHelper.sol
+++ src/libraries/TransferHelper.sol
@@ -39,20 +39,4 @@ library TransferHelper {
     ) internal {
         ERC1155(token).safeTransferFrom(from, to, id, amount, "");
     }
-
-    /// @notice Transfers a set of ERC1155 tokens
-    /// @param token    - The contract address of the token to be transferred
-    /// @param from     - The originating address from which the tokens will be transferred
-    /// @param to       - The destination address of the transfer
-    /// @param ids      - The tokenId of the token to be transferred
-    /// @param amounts  - The amount to be transferred
-    function _batchTransferFromERC1155(
-        address token,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts
-    ) internal {
-        ERC1155(token).safeBatchTransferFrom(from, to, ids, amounts, "");
-    }
 }
