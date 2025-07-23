f0f8edd2fcd3b43f0bf58fd371aa04ebbd94aeea
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-07-23 17:27:24 -0400

    fix: fmt

--- src/interfaces/IFeeModule.sol
+++ src/interfaces/IFeeModule.sol
@@ -8,7 +8,9 @@ interface IFeeModuleEE {
     event FeeWithdrawn(address token, address to, uint256 id, uint256 amount);
 
     /// @notice Emitted when fees are refunded to the order maker
-    event FeeRefunded(bytes32 indexed orderHash, address indexed token, address indexed to, uint256 id, uint256 amount, uint8 trader);
+    event FeeRefunded(
+        bytes32 indexed orderHash, address indexed token, address indexed to, uint256 id, uint256 amount, uint8 trader
+    );
 }
 
 interface IFeeModule is IFeeModuleEE {
