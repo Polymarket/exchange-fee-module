d052d9f2cd6fe218d6308c19314ff6f320d301f6
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-07-23 18:15:10 -0400

    fix: fmt

--- src/scripts/deploy/DeployFeeModule.s.sol
+++ src/scripts/deploy/DeployFeeModule.s.sol
@@ -13,29 +13,31 @@ contract DeployFeeModule is Script {
     /// @param exchange - The CTFExchange address
     function run(address admin, address exchange) public returns (address module) {
         vm.startBroadcast();
-        
+
         FeeModule feeModule = new FeeModule(exchange);
 
         // Add admin auth to the Admin address
         feeModule.addAdmin(admin);
-        
+
         // revoke deployer's auth
         feeModule.renounceAdmin();
-        
+
         module = address(feeModule);
 
         vm.stopBroadcast();
 
-        if(!_verifyStatePostDeployment(admin, exchange, module)){
-            revert("state verification post deployment failed");
-        }
+        if (!_verifyStatePostDeployment(admin, exchange, module)) revert("state verification post deployment failed");
     }
 
-    function _verifyStatePostDeployment(address admin, address exchange, address feeModule) internal view returns (bool) {
+    function _verifyStatePostDeployment(address admin, address exchange, address feeModule)
+        internal
+        view
+        returns (bool)
+    {
         FeeModule module = FeeModule(feeModule);
-        
+
         if (module.isAdmin(msg.sender)) revert("Deployer admin not renounced");
-        if (!module.isAdmin(admin)) revert("FeeModule admin not set");        
+        if (!module.isAdmin(admin)) revert("FeeModule admin not set");
         if (address(module.exchange()) != exchange) revert("Unexpected exchange set on the FeeModule");
 
         return true;
